7ff9602ac141677a0a8c765f312b41ec7adfbb6d
Tenebrae <80919466+ZodiackiIler@users.noreply.github.com>
2023-07-04 14:34:36 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+import asyncio
 from aiogram.utils import executor
-from config import dp
+from handlers.tonbalance import dp
+from misc.transaction import start 
 
 async def on_startup(_):
     print('Бот вышел в онлайн')
@@ -8,4 +10,6 @@ from handlers import tonbalance
 tonbalance.register_handlers_ton(dp)
 
 if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(start())
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
