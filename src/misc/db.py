b02495ad5f0ab5a1bd8e9de06fda2a73f9b27295
Tenebrae <80919466+ZodiackiIler@users.noreply.github.com>
2023-07-04 12:16:49 +0300

    Update db.py

--- src/misc/db.py
+++ src/misc/db.py
@@ -14,8 +14,22 @@ db_config = {
 
 class Database:
     def __init__(self, db_config):
-        self.connection = mysql.connector.connect(**db_config)
-        self.cursor = self.connection.cursor()
+        try:
+            self.connection = mysql.connector.connect(**db_config)
+            self.cursor = self.connection.cursor()
+
+            # Create the "users" table if it doesn't exist
+            self.cursor.execute(
+                '''
+                CREATE TABLE IF NOT EXISTS users (
+                    user_id INT PRIMARY KEY,
+                    username VARCHAR(255),
+                    toncoin DECIMAL(18, 9) DEFAULT 0
+                )
+                '''
+            )
+        except mysql.connector.Error as error:
+            print("Ошибка подключения к базе данных:", error)
 
     def check_user(self, uid):
         self.cursor.execute(f'SELECT * FROM users WHERE user_id = {uid}')
@@ -25,6 +39,9 @@ class Database:
         return False
     
     def get_tonbalance(self, uid):
+        if not self.connection.is_connected():
+            self.connection.reconnect()
+            self.cursor = self.connection.cursor()
         self.cursor.execute(f'SELECT toncoin FROM users WHERE user_id = {uid}')
         balance = self.cursor.fetchone()[0]
         return balance
@@ -33,15 +50,17 @@ class Database:
         self.cursor.execute(f'UPDATE users SET toncoin= toncoin + {amount} WHERE user_id = {uid}')
         self.connection.commit()
 
-    def user_exists(self, user_id):
+    def user_exists(self, uid):
         with self.connection:
             self.cursor.execute(f'SELECT * FROM users WHERE user_id = {uid}')
             result = self.cursor.fetchall()
             return bool(len(result))
 
-    def add_user(self, user_id, username, toncoin):
+    def add_user(self, uid, username):
         with self.connection:
-            query = "INSERT INTO users (user_id, username, toncoin) VALUES (%s, %s, %s)"
-            values = (user_id, username, toncoin)
+            query = "INSERT INTO users (user_id, username) VALUES (%s, %s)"
+            values = (uid, username)
             self.cursor.execute(query, values)
             self.connection.commit()
+
+db = Database(db_config)
